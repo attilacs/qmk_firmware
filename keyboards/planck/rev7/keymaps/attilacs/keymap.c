d4caecaf2aaf32c21aca3a8c7f219b5fc8b9d00a
attilacs <csanyiattila23@gmail.com>
2024-09-08 10:18:18 +0200

    Create tap dance gui key

--- keyboards/planck/rev7/keymaps/attilacs/keymap.c
+++ keyboards/planck/rev7/keymaps/attilacs/keymap.c
@@ -89,16 +89,20 @@ const uint32_t PROGMEM unicode_map[] = {
 
 /* Tap dance */
 #define TDALT TD(TD_ALT)
+#define TDGUI TD(TD_GUI)
 
 // Tap Dance declarations
 enum {
     TD_ALT,
+    TD_GUI,
 };
 
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Alt, twice for AltGr
     [TD_ALT] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT),
+    // Tap once for Gui, twice for App
+    [TD_GUI] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_APP),
 };
 
 /* clang-format off */
@@ -112,14 +116,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  |TDAlt |      |Lower |Space | Nums |Raise | Left | Down |  Up  |Right |
+ * | Ctrl |TDGUI |TDAlt |      |Lower |Space | Nums |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
     NAV,     KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LGUI, TDALT,   XXXXXXX, LOWER,   KC_SPC,  NUMBERS, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, TDGUI,   TDALT,   XXXXXXX, LOWER,   KC_SPC,  NUMBERS, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Qwerty
@@ -130,14 +134,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  |TDAlt |      |Lower |Space | Nums |Raise | Left | Down |  Up  |Right |
+ * | Ctrl |TDGUI |TDAlt |      |Lower |Space | Nums |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LGUI, TDALT,   XXXXXXX, LOWER,   KC_SPC,  NUMBERS, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, TDGUI,   TDALT,   XXXXXXX, LOWER,   KC_SPC,  NUMBERS, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
